6aec4cbc1c2d8dacc4181c01c5232b13cb1f9ea3
Sarah Razanatsoa <sarah.raz@onja.org>
2020-11-09 15:27:12 +0300

    add the first array of question

---
+++ questions.js
@@ -0,0 +1,112 @@
+ export default [
+    {
+        question: "Kabul is the capital of",
+        answers: [
+            {
+                country: 'France',
+                isTrue: false
+            },
+            {
+                country: 'Afghanistan',
+                isTrue: true
+            },
+            {
+                country: 'Palau',
+                isTrue: false
+            },
+            {
+                country: 'Philipine',
+                isTrue: false
+            }
+
+        ]
+    },
+    {
+        question: "Antananarivo is the capital of",
+        answers: [
+            {
+                country: 'Madagascar',
+                isTrue: true
+            },
+            {
+                country: 'Afghanistan',
+                isTrue: false
+            },
+            {
+                country: 'Palau',
+                isTrue: false
+            },
+            {
+                country: 'Philipine',
+                isTrue: false
+            }
+
+        ]
+    },
+    {
+        question: "Mariehamn is the capital of",
+        answers: [
+            {
+                country: 'Malawi',
+                isTrue: false
+            },
+            {
+                country: 'Åland Islands',
+                isTrue: true
+            },
+            {
+                country: 'Maldives',
+                isTrue: false
+            },
+            {
+                country: 'Morocco',
+                isTrue: false
+            }
+
+        ]
+    },
+    {
+        question: "Pago Pago is the capital of",
+        answers: [
+            {
+                country: 'American Samoa',
+                isTrue: true
+            },
+            {
+                country: 'Afghanistan',
+                isTrue: false
+            },
+            {
+                country: 'Palau',
+                isTrue: false
+            },
+            {
+                country: 'Philipine',
+                isTrue: false
+            }
+
+        ]
+    },
+    {
+        question: "Luanda is the capital of",
+        answers: [
+            {
+                country: 'Angola',
+                isTrue: true
+            },
+            {
+                country: 'Mozambique',
+                isTrue: false
+            },
+            {
+                country: 'Nigeria',
+                isTrue: false
+            },
+            {
+                country: 'Morocco',
+                isTrue: false
+            }
+
+        ]
+    }
+]
